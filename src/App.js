eca381388ef1bf695109575b0ccb7f6e5a6077d1
panley <panley@gamersafer.com>
2022-12-17 15:17:54 +0000

    ???

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import WorkList from './components/workList.js'
+import Header from './components/header';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <WorkList />
     </div>
   );
