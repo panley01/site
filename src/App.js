85d6114ca3effc17a5003ae2b90889b0f8e6a0d6
panley <panley@gamersafer.com>
2022-12-17 20:53:11 +0000

    ???

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Header from './components/header';
 function App() {
   return (
     <div className="App">
-      <img style={{width: '100vw'}} src={`${process.env.PUBLIC_URL}/wave.svg`}></img> => <img style={{width: '100vw'}} src={`${process.env.PUBLIC_URL}/wave.svg`} />
+      <img style={{width: '100vw'}} src={`${process.env.PUBLIC_URL}/wave.svg`} />
       <Header />
       <WorkList />
     </div>
