92ea02fa8206fcc985ae3142ff391a844e58e055
panley <panley@gamersafer.com>
2022-12-17 02:28:30 +0000

    ???

--- src/components/workEntry.js
+++ src/components/workEntry.js
@@ -16,7 +16,8 @@ function WorkEntry({entry}) {
         width="100"
         height="100"
         alt=""
-        style={{transform: `${isShown ? 'scale(1.5,1.5)' : 'scale(1,1)'}`, float: "left", margin: "0 15px 0 0", "transition-duration": "1s;"}}
+        style={{transform: `${isShown ? 'scale(1.5,1.5)' : 'scale(1,1)'}`}}
+        id="imageContainer"
       />
       <h2>{entry.company}{' • '}{entry.title}</h2>
       <h3>{`${startDate.toDateString()} - ${((endDate > today) ? 'Current' : endDate.toDateString())}`}</h3>
