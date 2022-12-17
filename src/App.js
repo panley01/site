7df38c76c63bd0cf3fe6e4a68cb84381b3ede81d
panley <panley@gamersafer.com>
2022-12-17 14:19:08 +0000

    ???

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 import WorkList from './components/workList.js'
 
 function App() {
   return (
     <div className="App">
-      <img src={logo} className="App-logo" alt="logo" />
       <WorkList />
     </div>
   );
