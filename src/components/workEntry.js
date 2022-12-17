f1658026f6a218bf258c3d405bd8b3275f343f88
panley <panley@gamersafer.com>
2022-12-17 01:41:00 +0000

    fixes

--- src/components/workEntry.js
+++ src/components/workEntry.js
@@ -1,24 +1,24 @@
 import React from 'react';
 import '../css/workEntry.css'
 
-function workHistory(props) {
-  const { company, title, image, started, ended, description } = props
-  const startDate = new Date(started)
-  const endDate = new Date(ended)
+function workEntry({entry}) {
+  const startDate = new Date(entry.started)
+  const endDate = new Date(entry.ended)
+  const today = new Date()
   return (
-    <div>
+    <div id="embedHolder">
       <img
-        src={`${process.env.PUBLIC_URL}images/${image}`}
+        src={`${process.env.PUBLIC_URL}images/${entry.image}`}
         width="300"
         height="300"
         alt=""
         id="workImage"
       />
-      <h1>{company}</h1>
-      <h2>{title}{'•'}{`${startDate.toDateString()} - ${endDate.toDateString()}`}</h2>
-      <div>{description}</div>
+      <h2>{entry.company}{' • '}{entry.title}</h2>
+      <h3>{`${startDate.toDateString().split().slice(1).join(' ')} - ${((endDate > today) ? 'Current' : endDate.toDateString().split().slice(1).join(' '))}`}</h3>
+      <div>{entry.description}</div>
     </div>
   )
 }
 
-export default workHistory
+export default workEntry
