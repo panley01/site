14b2470512d1ae192a791ddf05b23d2cb554fd84
panley <panley@gamersafer.com>
2022-12-18 21:57:16 +0000

    ???

--- src/components/contact.js
+++ src/components/contact.js
@@ -8,7 +8,7 @@ function Contact() {
     return (
       <div class="contactcontainer">
         <h1 class="contactheader">
-            Contact me <a href=''>via Discord <FaDiscord /></a> or <a href=''>send an email to p@nley.contact <HiOutlineMail /></a>
+            Contact me <a href='https://contact.panley01.workers.dev' class="contactheader">via Discord <FaDiscord /></a> or <a href='mailto:p@nley.contact?subject=Contact request from pa.nley.contact!' class="contactheader">send an email to p@nley.contact <HiOutlineMail /></a>
         </h1>
       </div>
     );
