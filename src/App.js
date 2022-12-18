6d4d0cd5f07a77ba94d84822c6e1853492872df3
panley <panley@gamersafer.com>
2022-12-18 00:17:44 +0000

    ???

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import WorkList from './components/workList.js'
-import Header from './components/header';
+import Header from './components/header'
+import Tagline from './components/taglines';
 
 function App() {
   return (
     <div className="App">
       <img style={{width: '100vw'}} src={`${process.env.PUBLIC_URL}/wave.svg`} />
       <Header />
+      <Tagline />
       <WorkList />
     </div>
   );
---
+++ src/components/taglines.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import '../css/header.css'
+import lines from '../data/taglines.json'
+
+function Tagline() {
+    const [currentLine, setCurrentLine] = React.useState(0);
+    const [line, setLine] = React.useState(lines[0]); // is the default value
+    
+    const incrementCurrentLine = () => {
+      if (currentLine < lines.length - 1) {
+        setCurrentLine(currentLine + 1);
+      } else {
+        // reset to 1 if reached the end
+        setCurrentLine(0);
+      }
+    };
+    
+    return (
+      <div class="container">
+        <h1 class="header"
+          onMouseEnter={() => {
+            incrementCurrentLine();
+            setLine(lines[currentLine]);
+          }}>
+            And I'm 
+          <span class="suffix">{line}</span>
+        </h1>
+      </div>
+    );
+}
+
+export default Tagline
