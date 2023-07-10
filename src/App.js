f6dfb1ee42747d67fa537e7529da5cc54b506ef3
panley01 <52224740+panley01@users.noreply.github.com>
2023-07-10 16:16:21 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -22,6 +22,7 @@ function App() {
         <div>
           <h1 style={{ color: 'white' }}>Projects</h1>
           <ProjectsList />
+          <h1 style={{ color: 'white' }}>Contact</h1>
           <SocialsList />
         </div>
       </div>
