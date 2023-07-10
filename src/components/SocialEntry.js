e8f8494137233d6a562f5d39d40683704336e1e1
panley01 <52224740+panley01@users.noreply.github.com>
2023-07-10 15:54:00 +0100

    Create SocialEntry.js

---
+++ src/components/SocialEntry.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import '../css/workEntry.css'
+import { FaDiscord, FaGithub, FaInstagram, FaLinkedin, FaReddit, FaSteam, FaTwitch, FaTwitter, FaYoutube } from 'react-icons/fa'
+import { HiOutlineMail } from 'react-icons/hi'
+import { SiHackerone } from 'react-icons/si'
+
+function SocialEntry({entry}) {
+  
+    const iconMapping = {
+        twitter: <FaTwitter />,
+        discord: <FaDiscord />,
+        github: <FaGithub />,
+        instagram: <FaInstagram />,
+        reddit: <FaReddit />,
+        youtube: <FaYoutube />,
+        steam: <FaSteam />,
+        hackerone: <SiHackerone />,
+        twitch: <FaTwitch />,
+        linkedin: <FaLinkedin />,
+        email: <HiOutlineMail />
+    }
+    
+  const [isShown, setIsShown] = useState(false)
+  
+  const startDate = new Date(entry.started * 1000)
+  const endDate = new Date(entry.ended * 1000)
+  const today = new Date()
+  const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+
+  return (
+    <div id="embedHolder" onMouseEnter={() => {console.log("entry"); setIsShown(true)}} onMouseLeave={() => {console.log("exit"); setIsShown(false)}}>
+      <a href={entry.link}><h2>{((iconMapping[entry.site]) ? iconMapping[entry.site] : '')}{entry.site}{' • '}{entry.prefix}{'panley'}{entry.suffix}</h2><>
+    </div>
+  )
+}
+
+export default SocialEntry
