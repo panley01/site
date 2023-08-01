560bcb540df638dfcd6fc4411bd6a96ab4a8b839
JohnyTheCarrot <tuurmartens4@gmail.com>
2023-08-01 17:32:25 +0200

    comment out profile placement

--- src/App.js
+++ src/App.js
@@ -7,41 +7,31 @@ import Header from './components/header'
 import { FaGithub, FaCloudflare  } from 'react-icons/fa'
 import DiscordProfile from "./components/discord_profiles/DiscordProfile.tsx";
 
-const discordUser = {
-  "id": "249287049482338305",
-  "username": "panley",
-  "global_name": "Panley",
-  "avatar": "842c666ed575d827f4b5d1e11c1dfb54",
-  "discriminator": "0",
-  "public_flags": 4588096,
-  "flags": 4588128,
-  "banner": "fcb28694fceb7107e12700b26238248e",
-  "theme_colors": [13594997, 10193583],
-  "bio": "I make things on Discord for a living.\n\nhttps://pa.nley.contact",
-  "locale": "en-GB",
-  "mfa_enabled": true,
-  "premium_type": 2,
-  "avatar_decoration": null,
-  "verified": true,
-  "theme": "light",
-}
+// const discordUser = {
+//   "id": "249287049482338305",
+//   "username": "panley",
+//   "global_name": "Panley",
+//   "avatar": "842c666ed575d827f4b5d1e11c1dfb54",
+//   "discriminator": "0",
+//   "public_flags": 4588096,
+//   "flags": 4588128,
+//   "banner": "fcb28694fceb7107e12700b26238248e",
+//   "theme_colors": [13594997, 10193583],
+//   "bio": "I make things on Discord for a living.\n\nhttps://pa.nley.contact",
+//   "locale": "en-GB",
+//   "mfa_enabled": true,
+//   "premium_type": 2,
+//   "avatar_decoration": null,
+//   "verified": true,
+//   "theme": "light",
+// }
 
 function App() {
   return (
     <div className="App">
       <img style={{width: '100vw'}} alt={'A white and black background seperated horizontally by a wavy line, white on top black on bottom. On the white section is the text: "Hi, I\'m".'} src={`${process.env.PUBLIC_URL}/wave.svg`} />
       <Header />
-      <div style={{
-        display: 'flex',
-        flexDirection: 'row',
-        justifyContent: 'center',
-        gap: '1rem',
-        marginTop: '1rem',
-        marginBottom: '1rem',
-      }}>
-        <DiscordProfile discordUser={{...discordUser, theme: 'light'}}/>
-        <DiscordProfile discordUser={{...discordUser, theme: 'dark'}}/>
-      </div>
+      {/*<DiscordProfile discordUser={discordUser}/>*/}
       <div className='textcontainer'>
         <div>
           <p style={{ color: 'white' }}>{'Hey there, I’m Panley! I lead a team of veteran developers with years of experience bringing apps to life on Discord! I’ve worked with all sorts of brands in collaboration with Discord to bring rich experiences to the platform. I’ve successfully worked with strategic partners for several important brand moments led by Discord. I consistently bring high quality applications to life on Discord, bringing meaningful ways for your fans to engage with your brand & convert into potential customers. I aim to make your ideas into a strong reality. Thanks to my dedicated i18n and QA teams, I can help you launch across regions bug-free. My standard stack runs on Cloudflare for infinite scalability - no downtime! If you\'re interested in my work, please feel free to contact me for more information or a quote! You can find my contact information below.'}</p>
