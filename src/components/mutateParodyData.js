7428d3427aac80bc614c4234d654b3378c289ea9
Panley01 <panley@pnly.io>
2024-08-08 03:46:14 +0100

    add parody

--- src/components/mutateParodyData.js
+++ src/components/mutateParodyData.js
@@ -4,7 +4,7 @@ export function mutateWorkEntry(data) {
     data.description = ((data.description == '') ? 'OpenAI API error: 500 Internal Server Error' : `I'm sorry as a large language model I am unable to help you with ${data.description}`)
     const titles = ['Deep Tech AI Analyst', 'Market Guru Master Investor', 'Founder CEO Co-Founder Executive', 'Chief Fun Officer', 'OpenAI API Wrapper Developer']
     data.title = titles[Math.floor(Math.random()*titles.length)]
-    const sellouts = ['ACQUIRED', 'RAISED', 'VALUED', 'SEEDED']
+    const sellouts = ['💰 ACQUIRED', '💸 RAISED', '🤑 VALUED', '💦 SEEDED']
     data.company += ` (${sellouts[Math.floor(Math.random()*sellouts.length)]} $${(Math.floor(1000000 + Math.random() * 9000000)).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")})`
     return data
 }
