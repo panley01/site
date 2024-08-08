346c9672010f4bf72c398b33421de895339d2d99
Panley01 <panley@pnly.io>
2024-08-08 04:18:21 +0100

    ok we clean up

--- src/components/mutateParodyData.js
+++ src/components/mutateParodyData.js
@@ -1,7 +1,7 @@
 
 
 export function mutateWorkEntry(data) {
-    data.description = ((data.description == '') ? 'OpenAI API error: 500 Internal Server Error' : `I'm sorry as a large language model I am unable to help you with ${data.description}`)
+    data.description = ((data.description == undefined) ? 'OpenAI API error: 500 Internal Server Error' : `I'm sorry as a large language model I am unable to help you with summarizing your experience as a ${data.title} at ${data.company}`)
     const titles = ['Deep Tech AI Analyst', 'Market Guru Master Investor', 'Founder CEO Co-Founder Executive', 'Chief Fun Officer', 'OpenAI API Wrapper Developer']
     data.title = titles[Math.floor(Math.random()*titles.length)]
     const sellouts = ['💰 ACQUIRED', '💸 RAISED', '🤑 VALUED', '💦 SEEDED']
