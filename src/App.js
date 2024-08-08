cac65572e761598159c30ebcc954a5ed31e523ec
Panley01 <panley@pnly.io>
2024-08-08 05:46:55 +0100

    bonzi can't autoplay :(

--- src/App.js
+++ src/App.js
@@ -37,8 +37,7 @@ function App() {
     audio.play()
   }
   return (
-    <div className="App" onClick={start}>
-      {((isParodySite) ? <audio id="audio" loop autoplay src={`${process.env.PUBLIC_URL}/bonzi.mp3`}></audio> : '')}
+    <div className="App" onClick={((isParodySite) ? start : undefined)}>
       <img style={{width: '100vw'}} alt={'A white and black background seperated horizontally by a wavy line, white on top black on bottom. On the white section is the text: "Hi, I\'m".'} src={((isParodySite) ? `${process.env.PUBLIC_URL}/awfulaibanner.jpg` : `${process.env.PUBLIC_URL}/wave.svg`)} />
       <Header />
       <div className='textcontainer'>
