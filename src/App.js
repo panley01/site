240964de30791c4a0f337ccbada5e82882829060
Panley01 <panley@pnly.io>
2024-08-08 03:54:26 +0100

    debugging with the console on prod

--- src/App.js
+++ src/App.js
@@ -27,24 +27,26 @@ const discordUser = {
 }
 
 function App() {
-  console.log(process.env.PUBLIC_URL)
+  console.log(window.location.href)
+  const isParodySite = (new URL(window.location.href)).hostname == 'levels.pnly.io'
+  console.log(isParodySite)
   return (
     <div className="App">
       <img style={{width: '100vw'}} alt={'A white and black background seperated horizontally by a wavy line, white on top black on bottom. On the white section is the text: "Hi, I\'m".'} src={`${process.env.PUBLIC_URL}/wave.svg`} />
       <Header />
       <div className='textcontainer'>
         <div>
-          <p style={{ color: 'white' }}>{((process.env.PUBLIC_URL.includes('levels.pnly.io')) ? 'Panley-40: An Acid-Reducing Medication Panley-40 is a medication that primarily treats acid-related issues in the stomach and intestines. It contains the active ingredient pantoprazole, which works by reducing the amount of acid produced in your stomach. Common Uses of Panley-40: Peptic ulcer disease, Acid reflux (GERD), Heartburn. How Panley-40 Works, Pantoprazole, the active ingredient in Panley-40, blocks the production of acid in your stomach. This helps to relieve symptoms and promote healing of damaged tissues. Important Note: While I\'ve provided general information, it\'s crucial to consult with a healthcare professional for personalized advice regarding Panley-40 or any medication. They can provide specific guidance based on your medical history and condition. Would you like to know more about the side effects, precautions, or how to take Panley-40?' : 'Hey there, I’m Panley! I lead a team of veteran developers with years of experience bringing apps to life on Discord! I’ve worked with all sorts of brands in collaboration with Discord to bring rich experiences to the platform. I’ve successfully worked with strategic partners for several important brand moments led by Discord. I consistently bring high quality applications to life on Discord, bringing meaningful ways for your fans to engage with your brand & convert into potential customers. I aim to make your ideas into a strong reality. Thanks to my dedicated i18n and QA teams, I can help you launch across regions bug-free. My standard stack runs on Cloudflare for infinite scalability - no downtime! If you\'re interested in my work, please feel free to contact me for more information or a quote! You can find my contact information below.')}</p>
+          <p style={{ color: 'white' }}>{((isParodySite) ? 'Panley-40: An Acid-Reducing Medication Panley-40 is a medication that primarily treats acid-related issues in the stomach and intestines. It contains the active ingredient pantoprazole, which works by reducing the amount of acid produced in your stomach. Common Uses of Panley-40: Peptic ulcer disease, Acid reflux (GERD), Heartburn. How Panley-40 Works, Pantoprazole, the active ingredient in Panley-40, blocks the production of acid in your stomach. This helps to relieve symptoms and promote healing of damaged tissues. Important Note: While I\'ve provided general information, it\'s crucial to consult with a healthcare professional for personalized advice regarding Panley-40 or any medication. They can provide specific guidance based on your medical history and condition. Would you like to know more about the side effects, precautions, or how to take Panley-40?' : 'Hey there, I’m Panley! I lead a team of veteran developers with years of experience bringing apps to life on Discord! I’ve worked with all sorts of brands in collaboration with Discord to bring rich experiences to the platform. I’ve successfully worked with strategic partners for several important brand moments led by Discord. I consistently bring high quality applications to life on Discord, bringing meaningful ways for your fans to engage with your brand & convert into potential customers. I aim to make your ideas into a strong reality. Thanks to my dedicated i18n and QA teams, I can help you launch across regions bug-free. My standard stack runs on Cloudflare for infinite scalability - no downtime! If you\'re interested in my work, please feel free to contact me for more information or a quote! You can find my contact information below.')}</p>
         </div>
       </div>
       <div className='embedcontainer'>
         <div>
           <h1 style={{ color: 'white' }}>Work History</h1>
-          <WorkList isParodySite = {process.env.PUBLIC_URL.includes('levels.pnly.io')}/>
+          <WorkList isParodySite = {isParodySite}/>
         </div>
         <div>
           <h1 style={{ color: 'white' }}>Projects</h1>
-          <ProjectsList isParodySite = {process.env.PUBLIC_URL.includes('levels.pnly.io')}/>
+          <ProjectsList isParodySite = {isParodySite}/>
           <h1 style={{ color: 'white' }}>Discord</h1>
           <div style={{ display: 'flex', 'justify-content': 'center' }}>
             <DiscordProfile discordUser={discordUser}/>
@@ -57,8 +59,8 @@ function App() {
       </div>
       <div className='footercontainer'>
         <div>
-          <a target={'_blank'} href={'https://github.com/panley01/site'} style={{ color: 'white' }}><h1 style={{ color: 'white' }}>{<FaGithub />}{((process.env.PUBLIC_URL.includes('levels.pnly.io')) ? '  This website is a parody!' : '  This website is OSS!')}</h1></a>
-          <a target={'_blank'} href={'https://pages.cloudflare.com/'} style={{ color: 'white' }}><h2 style={{ color: 'white' }}>{<FaCloudflare />}{((process.env.PUBLIC_URL.includes('levels.pnly.io')) ? '  Hi Twitter.' : '  And hosted using Cloudflare Pages')}</h2></a>
+          <a target={'_blank'} href={'https://github.com/panley01/site'} style={{ color: 'white' }}><h1 style={{ color: 'white' }}>{<FaGithub />}{((isParodySite) ? '  This website is a parody!' : '  This website is OSS!')}</h1></a>
+          <a target={'_blank'} href={'https://pages.cloudflare.com/'} style={{ color: 'white' }}><h2 style={{ color: 'white' }}>{<FaCloudflare />}{((isParodySite) ? '  Hi Twitter.' : '  And hosted using Cloudflare Pages')}</h2></a>
         </div>
       </div>
     </div>
